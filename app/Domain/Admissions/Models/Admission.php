bbde4f99111e0ab477fae1dfc2d1fb0c1f3e29eb
bekhruzsalimovsobirovich <bekhruzsalimovsobirovich@gmail.com>
2024-03-29 10:23:27 +0500

    updated

--- app/Domain/Admissions/Models/Admission.php
+++ app/Domain/Admissions/Models/Admission.php
@@ -2,10 +2,12 @@
 
 namespace App\Domain\Admissions\Models;
 
+use App\Domain\Files\Models\File;
 use App\Domain\Patients\Models\Patient;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Admission extends Model
 {
@@ -23,5 +25,13 @@ class Admission extends Model
     public function patient(): BelongsTo
     {
         return $this->belongsTo(Patient::class);
+    }
+
+    /**
+     * @return MorphMany
+     */
+    public function files(): MorphMany
+    {
+        return $this->morphMany(File::class, 'fileable');
     }
 }
