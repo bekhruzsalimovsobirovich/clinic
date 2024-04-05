7cb9b0b88a6c6dd7e3ac402f99f012b09d999f9b
sukhrob salimov <sukhrobjansalimov@mail.ru>
2024-04-05 19:15:11 +0500

    update

--- app/Domain/Payments/Actions/StorePaymentAction.php
+++ app/Domain/Payments/Actions/StorePaymentAction.php
@@ -19,23 +19,48 @@ class StorePaymentAction
     {
         DB::beginTransaction();
         try {
-
             $payment = new Payment();
             $payment_history = new PaymentHistory();
 
-            $payment->patient_id = $dto->getPatientId();
-            $payment->admission_id = $dto->getAdmissionId();
-            $payment->status = $dto->getStatus();
-            $payment->pays = $dto->getPays();
-            $payment->save();
+            $current_payment = Payment::query()
+                ->where('patient_id',$dto->getPatientId())
+                ->where('admission_id',$dto->getAdmissionId())
+                ->first();
 
-            $payment_history->patient_id = $dto->getPatientId();
-            $payment_history->admission_id = $dto->getAdmissionId();
-            $payment_history->status = $dto->getStatus();
-            $payment_history->pays = $dto->getPays();
-            $payment_history->save();
-            DB::commit();
-            return $payment;
+            $current_payment_history = PaymentHistory::query()
+                ->where('patient_id',$dto->getPatientId())
+                ->where('admission_id',$dto->getAdmissionId())
+                ->first();
+
+            if($current_payment == null){
+                $payment->patient_id = $dto->getPatientId();
+                $payment->admission_id = $dto->getAdmissionId();
+                $payment->status = $dto->getStatus();
+                $payment->pays = $dto->getPays();
+                $payment->save();
+
+                $payment_history->patient_id = $dto->getPatientId();
+                $payment_history->admission_id = $dto->getAdmissionId();
+                $payment_history->status = $dto->getStatus();
+                $payment_history->pays = $dto->getPays();
+                $payment_history->save();
+                DB::commit();
+                return $payment;
+            }else{
+                if ($current_payment->pays != null) {
+                    $current_payment->pays = array_merge($current_payment->pays, $dto->getPays());
+                    $current_payment_history->pays = array_merge($current_payment_history->pays, $dto->getPays());
+                } else {
+                    $current_payment->pays = $dto->getPays();
+                    $current_payment_history->pays = $dto->getPays();
+                }
+                $current_payment->update();
+                $current_payment_history->update();
+                DB::commit();
+                return $current_payment;
+            }
+
+
         } catch (Exception $exception) {
             DB::rollBack();
             throw $exception;
